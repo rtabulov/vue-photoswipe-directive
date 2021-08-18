68f04d6ec56efe9e2a4e4a964d61f95ad5b1ddce
Rassul <rasul.tabulov@gmail.com>
2021-08-18 18:02:32 +0600

    wip

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+export default {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ["plugin:vue/essential", "@vue/standard"],
+  rules: {},
+  parserOptions: {
+    parser: "babel-eslint",
+  },
+};
